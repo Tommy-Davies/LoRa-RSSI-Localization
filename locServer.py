b3fe79331ae8906c101e8444f017e9ee82ad10ec
Tommy Davies <daviestho@gmail.com>
2021-11-21 15:32:13 -0500

    cleaned up

--- locServer.py
+++ locServer.py
@@ -6,28 +6,18 @@ import adafruit_rfm9x
 import statistics
 import math
 
-# Define radio parameters.
-RADIO_FREQ_MHZ = 900.0  # Frequency of the radio in Mhz. Must match your
-# module! Can be a value like 915.0, 433.0, etc.
+RADIO_FREQ_MHZ = 900.0 
 
-# Define pins connected to the chip.
-# set GPIO pins as necessary - this example is for Raspberry Pi
 CS = digitalio.DigitalInOut(board.CE1)
 RESET = digitalio.DigitalInOut(board.D25)
 
-# Initialize SPI bus.
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-# Initialze RFM radio
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
 rfm9x.tx_power = 23
-# enable CRC checking
 rfm9x.enable_crc = True
-# set delay before transmitting ACK (seconds)
 rfm9x.ack_delay = 0.05
-# set node addresses
 rfm9x.node = 4
 rfm9x.destination = 1
-# initialize counter
 counter = 0
 ack_failed_counter = 0
 
@@ -60,7 +50,6 @@ def handlePacket(rawPacket):
     return nodeAData, nodeBData, nodeCData
 
 def calcDistance(nodeA, nodeB, nodeC):
-    # print(nodeA)
     avgNodeA = 1.1
     avgNodeB = 1.1 
     avgNodeC = 1.1
@@ -88,7 +77,6 @@ def calcDistance(nodeA, nodeB, nodeC):
 
 
 
-# Wait to receive packets.
 print("Waiting for packets...")
 
 while True:
@@ -111,7 +99,4 @@ while True:
         print(packetData)
         if not rfm9x.send_with_ack(bytes("I don't know why but this is necessary", "UTF-8")):
             print("No Ack")
-     
 
-    # else:
-    #     print("none packet")
