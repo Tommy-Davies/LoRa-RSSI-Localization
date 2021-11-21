0124012b8617571a491b8532b75929379c87af16
Tommy Davies <daviestho@gmail.com>
2021-11-21 09:50:29 -0500

    added code for esp32 nodes

---
+++ RecieveNode/src/main.cpp
@@ -0,0 +1,76 @@
+
+#include <RHReliableDatagram.h>
+//#include <RHDatagram.h>
+#include <RH_RF95.h>
+#include <SPI.h>
+
+#define PORTABLE_ADDRESS 1 //Portable node
+#define NODEA 2            //esp1 (self)
+#define NODEB 3            //esp2 (self)
+#define NODEC 4            //raspberry pi
+
+#define NSS 5
+#define DIO0 26
+RH_RF95 driver(NSS, DIO0);
+
+RHReliableDatagram manager(driver, NODEB); //change this on upload
+
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial)
+    ; // Wait for serial port to be available
+  if (!manager.init())
+    Serial.println("init failed");
+  //    if (!manager2.init())
+  //        Serial.println("init 2 failed");
+  driver.setFrequency(900);
+  driver.setTxPower(23, false);
+}
+
+uint8_t data[] = "Loc Node";
+uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+
+void loop()
+{
+  // Serial.println("working");
+  uint8_t len = sizeof(buf);
+  uint8_t from;
+
+  if (manager.available())
+  {
+    // Wait for a message addressed to us from the client
+    uint8_t len = sizeof(buf);
+    uint8_t from;
+    if (manager.recvfromAck(buf, &len, &from))
+    {
+      Serial.print("got request from : 0x");
+      Serial.print(from, HEX);
+      Serial.print(": ");
+      Serial.println((char *)buf);
+
+      // Send a reply back to the originator client
+      if (!manager.sendtoWait(data, sizeof(data), from))
+        Serial.println("sendtoWait failed");
+    }
+  }
+  // uint8_t to;
+  // uint8_t id;
+  // uint8_t flags;
+
+  //  if (manager.available())
+  //  {
+  //    Serial.println("available");
+  //    if(driver.recv(buf,&len)){
+  //      Serial.println((char*)buf);
+  //    }
+  // if(manager.recvfrom(buf, &len, &from)){
+  //   Serial.print("got reply from : 0x");
+  //   Serial.print(from, HEX);
+  //   Serial.print(": ");
+  //   Serial.println((char*)buf);
+  // }
+  // delay(500);
+  //  }
+}
---
+++ localizedNode/src/sendtoNodeA.ino
@@ -0,0 +1,183 @@
+
+#include <RHReliableDatagram.h>
+#include <RH_RF95.h>
+#include <SPI.h>
+
+#define PORTABLE_ADDRESS 1 //self
+#define NODEA 2            //esp1
+#define NODEB 3            //esp2
+#define NODEC 4            //raspberry pi
+
+#define NSS 5
+#define DIO0 26
+
+RH_RF95 driver(NSS, DIO0);
+
+RHReliableDatagram manager(driver, NODEB);
+
+void setup()
+{
+  Serial.begin(115200);
+    // Serial.println("here");
+
+  if (!driver.init())
+  {
+    Serial.println("LoRa init failed. Check your connections.");
+    // while (true)
+    //   ;
+  } else {
+    Serial.println("LoRa init success");
+  }
+  // while (!Serial)
+  //   ; // Wait for serial port to be available
+  if (!manager.init())
+    Serial.println("init failed");
+  driver.setFrequency(900);
+  driver.setTxPower(23, false);
+}
+
+uint8_t data[] = "Ping";
+uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+
+void loop()
+{
+
+  uint8_t len = sizeof(buf);
+  uint8_t from;
+
+  String nodeAPacket = "";
+  String nodeBPacket = "";
+  String nodeCPacket = "";
+
+
+  //populate nodeA packet
+  nodeAPacket += ",NodeA,";
+  for (int i = 0; i < 10; i++)
+  {
+    if (manager.sendtoWait(data, sizeof(data), NODEA))
+    {
+      if (manager.recvfromAckTimeout(buf, &len, 1000, &from))
+      {
+        nodeAPacket += String(driver.lastRssi());
+
+        if (i < 9)
+        {
+          nodeAPacket += ",";
+        }
+        else
+        {
+          nodeAPacket += "EOD,";
+        }
+
+        // delay(50);
+      }
+    }
+    else
+    {
+      Serial.println("Packet loss A!");
+    }
+  }
+  nodeBPacket += ",NodeB,";
+
+  //populate nodeB packet
+  for (int i = 0; i < 10; i++)
+  {
+    if (manager.sendtoWait(data, sizeof(data), NODEB))
+    {
+      if (manager.recvfromAckTimeout(buf, &len, 1000, &from))
+      {
+        nodeBPacket += String(driver.lastRssi());
+
+        if (i < 10)
+        {
+          nodeBPacket += ",";
+        }
+        else
+        {
+          nodeBPacket += "EOD,";
+        }
+        // delay(50);
+      }
+    }
+    else
+    {
+      Serial.println("Packet loss B!");
+    }
+  }
+
+  nodeCPacket += ",NodeC,";
+
+  //populate nodeC packet
+  for (int i = 0; i < 10; i++)
+  {
+    Serial.println("send to c");
+    if (manager.sendtoWait(data, sizeof(data), NODEC))
+    {
+      if (manager.recvfromAckTimeout(buf, &len, 1000, &from))
+      {
+        nodeCPacket += String(driver.lastRssi());
+
+        if (i < 10)
+        {
+          nodeCPacket += ",";
+        }
+        else
+        {
+          nodeCPacket += "EOD,";
+        }
+
+        // delay(50);
+      }
+    }
+    else
+    {
+      Serial.println("Packet loss C!");
+    }
+  }
+
+  String eofStr = "EOF,";
+  int packetSize = nodeAPacket.length() + nodeBPacket.length() + nodeCPacket.length() + eofStr.length(); //packet sizes plus EOF
+  // uint8_t *packetData = (uint8_t *)malloc(packetSize*8);
+  String packetString = "";
+  packetString = nodeAPacket + nodeBPacket + nodeCPacket + eofStr;
+  uint8_t *packetData = (uint8_t*)packetString.c_str();
+
+  // packetString.getBytes(packetData, packetString.length() + 1);
+
+ 
+  // Serial.println((packetData));
+  //send packets to data server
+  for(int i = 0; i < packetString.length(); i++){
+    Serial.print((char)packetData[i]);
+  }
+  // Serial.println(packetString.c_str()); 
+  Serial.println(sizeof(packetString));
+  Serial.println(packetString.length());
+  uint8_t test[] = "test";
+  if(manager.sendtoWait(packetData, packetString.length(), NODEC)){
+    if (manager.recvfromAckTimeout(buf, &len, 1000, &from))
+    {
+      Serial.println("Transmission successful. Dumping data.");
+    }
+
+  }
+  // manager.sendtoWait((uint8_t *)packetString, sizeof(packetData), NODEC);
+
+  // if (manager.sendtoWait(packetData, sizeof(packetData), NODEC))
+  // {
+    // if (manager.recvfromAckTimeout(buf, &len, 5000, &from))
+    // {
+    //   Serial.println("Transmission successful. Dumping data.");
+
+    //   memset(packetData, 0, sizeof(packetData));
+    //   nodeAPacket = "";
+    //   nodeBPacket = "";
+    //   nodeCPacket = "";
+    //   j = 0;
+
+    // }
+  // }
+  // free(packetData);
+
+  // delay(500);
+}
